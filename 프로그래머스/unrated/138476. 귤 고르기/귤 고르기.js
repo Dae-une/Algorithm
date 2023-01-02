b5f8f69b5191a5d9554aadd3ea9cc06d77af6890
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-01-02 14:29:57 +0900

    [unrated] Title: 귤 고르기, Time: 68.81 ms, Memory: 50.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,16 @@
+function solution(k, tangerine) {
+    let answer =0;
+    const obj={}
+    
+    tangerine.forEach((t)=> obj[t]= (obj[t] || 0) +1)
+    const arr =Object.values(obj).sort((a,b)=>b-a)
+    
+    for(let i =0; i<arr.length;i++){
+        answer++
+        if(k > arr[i]){
+            k -=arr[i]    
+        } 
+        else break;
+    }
+    return answer
+}
