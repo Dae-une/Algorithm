3a242ce0cf18fcbc15289aadb65546e250cf65a4
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-02-10 14:30:39 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 78.35 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,23 +1,12 @@
 function solution(elements) {
-    const sumSet =new Set();
-    
-    const elemLength = elements.length;
-    for(let i =1;i<=elemLength;i++){
-        let sum=0;
-        for(let j =0;j<elemLength;j++){
-            if(j===0){
-                for(let k =0;k<i;k++){
-                    sum += elements[k];
-                }
-            }
-            else{
-                    sum -= elements[j-1]
-                    sum += elements[(j+i-1) % elemLength]
-                }
-                
-                sumSet.add(sum)
+    const circular = elements.concat(elements);
+    const set = new Set();
+    for (let i = 0; i < elements.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < elements.length; j++) {
+            sum += circular[i + j];
+            set.add(sum);
         }
     }
-    
-    return sumSet.size
+    return set.size;
 }
