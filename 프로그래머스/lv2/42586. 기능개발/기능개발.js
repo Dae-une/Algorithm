06e3f286cf5ba19b80e1fbea52553d24cadb1ea0
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-03-17 14:55:51 +0900

    [level 2] Title: 기능개발, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,56 +1,21 @@
 function solution(progresses, speeds) {
-    let date = [];
-    let answer =[]
-    for(let i =0; i< progresses.length; i++){
-        let count =0;
-        while(progresses[i] < 100){
-            progresses[i] = progresses[i] +(speeds[i])
-            count++
-        }
-        date.push(count)
-    }
-
-    while(date.length > 0){
-        let count=1;
-        for(let j =1; j<= date.length; j++){
-            if(date[0]>= date[j]){
-                count++
-            }else{
-                date.splice(0,count)
-                break;
-            }
-        }
-        answer.push(count)
-    }
-
+    var answer = [];
     
+    while(speeds.length){
+        for(let i =0; i<speeds.length;i++){
+            progresses[i] += speeds[i]
+        }
+        
+        
+        let deployCount=0;
+        while(progresses[0]>=100){
+            progresses.shift();
+            speeds.shift();
+            deployCount++
+        }
+        if(deployCount >0){
+            answer.push(deployCount)
+        }
+    }
     return answer;
 }
-
-
-
-// function solution(progresses, speeds) {
-//     var answer = [];
-
-//     while(speeds.length > 0) {
-//         // 개발
-//         for(let i in speeds) {
-//             if(progresses[i] < 100) {
-//                 progresses[i] += speeds[i];
-//             }
-//         }
-
-//         // 배포
-//         let deploy_count = 0;
-//         while(progresses[0] >= 100) {
-//             progresses.shift();
-//             speeds.shift();
-//             deploy_count++;
-//         }
-//         if(deploy_count > 0) {
-//             answer.push(deploy_count);
-//         }
-//     }
-
-//     return answer;
-// }
