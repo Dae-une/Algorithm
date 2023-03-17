6cf03d13c9addd1a9ece037dc16d205cd30ca3ec
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-03-17 13:08:01 +0900

    [level 2] Title: n^2 배열 자르기, Time: 6.10 ms, Memory: 43.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -0,0 +1,11 @@
+function solution(n, left, right) {
+    var answer = [];
+    
+      for (let i=left; i <= right; i++) {
+        const x = parseInt(i/n);	
+        const y = i%n;	
+        answer.push(Math.max(x, y) + 1)
+    }
+    
+    return answer;
+}
