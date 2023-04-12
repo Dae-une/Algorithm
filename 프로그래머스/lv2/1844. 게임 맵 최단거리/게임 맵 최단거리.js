682f8cbef3e5d8c46eca2c4d114a7b56b8ee7545
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-04-13 06:01:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 33.76 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,32 +1,33 @@
 function solution(maps) {
-    const row = maps.length -1
-    const col = maps[0].length-1
-    
-    const queue =[[0,0,1]]
-    const directions =[[1,0],[0,1],[-1,0],[0,-1]]
-    
+    const row = maps.length-1;
+    const col = maps[0].length-1;
+    const direction = [[0,1],[1,0],[0,-1],[-1,0]]
+    const queue = [[0,0,1]]
     while(queue.length){
-        let [y,x,count] = queue.shift()
+        const [y,x,count] = queue.shift();
         
-        if(y === row && x === col) return count;
+        if( y ===row && x === col) return count;
         
-        for(let i =0; i<4 ;i++){
-            const [dy,dx] =directions[i]
-            const nextY = dy+y
-            const nextX = dx+x
+        for(let i =0;i<4;i++){
+            const [dy,dx]=direction[i]
+            const nextY = dy+y;
+            const nextX = dx+x;
             
-            if(isRoad(nextY,nextX,row,col)) continue;
+            if(isNotRoad(nextX,nextY,row,col)){
+                continue;
+            }
             
-            if(maps[nextY][nextX] === 0 )continue;
-            maps[nextY][nextX] = 0
-            
+            if(maps[nextY][nextX] === 0){
+                continue;
+            }
+            maps[nextY][nextX] =0;
             queue.push([nextY,nextX,count+1])
         }
     }
-    return -1
-
+    return -1;
 }
 
-const isRoad =(nextY,nextX,row,col) => {
-    return nextY < 0 || nextX < 0 || nextY > row || nextX > col
+
+const isNotRoad =(nextX,nextY,row,col)=>{
+    return nextY <0 || nextX < 0 || nextX>col || nextY > row
 }
