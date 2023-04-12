6d0795c60793302694b418b9458ee3d491c80bc7
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-04-13 05:46:06 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -0,0 +1,24 @@
+function solution(n, computers) {
+    var answer = 0;
+    const visited =[];
+    
+
+    function dfs(i){
+        visited[i] =true;
+        for(let j =0;j<n;j++){
+            if(computers[i][j] === 1 && !visited[j]){
+                dfs(j);
+            }
+        }
+    }
+    
+    for(let i =0; i<n;i++){
+        if(!visited[i]){
+            dfs(i)
+            answer++;
+        }
+    }
+
+    
+    return answer;
+}
