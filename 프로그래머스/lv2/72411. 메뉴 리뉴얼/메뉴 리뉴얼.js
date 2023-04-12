4eec954fd2e9763e13b7d81f42dead942c46654f
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-04-12 15:30:44 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 24.48 ms, Memory: 45.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -0,0 +1,35 @@
+function solution(orders, course) {
+    var answer = [];
+    course.forEach((num)=>{
+        const menus = new Map();
+        orders.forEach((order)=>{
+            const combinations = getCombination(order.split('').sort(),num);
+            combinations.forEach((combs)=>{
+                const comb = combs.join('');
+                menus.set(comb,menus.has(comb) ? menus.get(comb)+1:1)
+            })
+        })
+        let populate =Math.max(...menus.values());
+        
+        menus.forEach((count,menu,map)=>{
+            if(count === populate && count >1) answer.push(menu)
+        })
+        
+    })
+    
+    
+    return answer.sort();
+}
+
+function getCombination (arr,num){
+    const results =[];
+    if (num ===1) return arr.map((v)=>[v]);
+    
+    arr.forEach((fixed,index,origin)=>{
+        const rest = origin.slice(index+1);
+        const combinations =getCombination(rest,num-1);
+        const attached = combinations.map((v)=>[fixed,...v]);
+        results.push(...attached)
+    })
+    return results
+}
