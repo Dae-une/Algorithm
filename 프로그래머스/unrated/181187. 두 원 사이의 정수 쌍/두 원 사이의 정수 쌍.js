9e788d7b91354d07e354583e0fdd672d68e275b2
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-04-19 23:52:31 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 17.46 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
@@ -0,0 +1,27 @@
+function solution(r1, r2) {
+    var answer = 0;
+    
+    let r2Has =0;
+    let r1Has =0;
+    
+    for(let x =1;x<r2;x++){
+        r2Has += getY(x,r2,'r2');
+    }
+    for (let x=1;x<r1;x++){
+        r1Has +=getY(x,r1,'r1');
+    }
+    
+    console.log(r2Has,r1Has)
+    
+    
+    return (r2Has-r1Has + r2-r1+1) * 4
+}
+
+function getY(x,r,rType){
+    const max =Math.sqrt(r *r - x*x);
+    const maxToInt = parseInt(max);
+    if(rType === 'r1' && max - maxToInt === 0){
+        return maxToInt -1;
+    }
+    return maxToInt
+}
