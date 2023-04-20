6e95c3a47edbba4fec248fe33381ea3ad2f1b8cc
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-04-20 14:29:58 +0900

    [unrated] Title: 요격 시스템, Time: 4.53 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.js
@@ -2,25 +2,24 @@ function solution(targets) {
     var answer = 0;
     targets.sort((a,b)=>{
         if(a[0] === b[0]){
-           return a[0] - b[0]
-        }else{
-            return a[1] - b[1]
+         return a[0] -b[0]
         }
+        return a[1]-b[1]
     })
     
-    let point = -1;
-    
+    console.log(targets)
+    let point =-1;
     for(let i =0;i<targets.length;i++){
         const left = targets[i][0]
         const right = targets[i][1]
         
         if(left >= point){
             answer++
-            point = right;
+            point =right
         }
+        
     }
-    
-    
-    
     return answer;
 }
+
+
