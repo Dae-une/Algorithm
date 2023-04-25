4e85cc264853159f5ea14871d26821315598c1a4
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-04-25 15:57:27 +0900

    [unrated] Title: 과제 진행하기, Time: 19.29 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.js
@@ -0,0 +1,21 @@
+function solution(plans) {
+    const stack =[]
+    
+    const sortedPlans= plans.map(([title,time,count])=>
+                                 [title,timeToMinute(time),Number(count)]).sort((a,b)=> a[1] -b[1])
+    while(sortedPlans.length){
+        const [title,time,count]= sortedPlans.shift();
+        
+        stack.forEach((val,idx)=>{
+            if(time < val[1])stack[idx][1] +=count;
+        })
+        
+        stack.push([title,time+count]);
+    }
+    return stack.sort((a,b)=>(a[1]-b[1])).map((el)=>el[0])
+}
+
+function timeToMinute(str){
+    const [hour,minute] = str.split(":").map((el)=>Number(el));
+    return hour * 60 + minute;
+}
