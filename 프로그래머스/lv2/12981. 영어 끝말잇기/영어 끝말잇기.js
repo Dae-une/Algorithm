112a1825106eed1a9133815dc452926ebbf5dba4
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-05-02 14:18:50 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,9 +1,11 @@
 function solution(n, words) {
-    let turn = 1
-    for(let i=1; i<words.length; i++){
-        let pass = (words[i][0] === words[i-1][words[i-1].length-1]) && !(words.slice(0, i).includes(words[i]))
-        if(i % n === 0) turn++
-        if(!pass) return [i % n + 1, turn]
+    let turn =1;
+    
+    for(let i =1; i <words.length;i++){
+     let pass = words[i][0] === words[i-1][words[i-1].length-1] && !(words.slice(0,i).includes(words[i]))
+     if( i % n === 0) turn ++;
+     if(!pass) return [i % n+1,turn]
     }
-    return [0,0];
+
+    return [0,0]
 }
