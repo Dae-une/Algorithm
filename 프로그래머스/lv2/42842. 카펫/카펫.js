109ec2e6f620a41a49494ed9497e6086f4fb1a0a
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-05-02 14:30:45 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,17 +1,16 @@
 function solution(brown, yellow) {
     var answer = [];
-    const size = brown + yellow
+    const size = brown + yellow;
     
-    answer = ratioList(size).filter((list)=> (list[0]+list[1]) *2 -4 ===brown)[0]
-    return answer
-
+    return ratioList(size).filter((list)=> (list[0] +list[1]) *2 -4 === brown)[0]
 }
 
-const ratioList=(size)=>{
-    let result =[]
-    for(let i =1; i<= Math.sqrt(size); i ++){
-     if(size % i === 0) result.push([size/i,i])
- }
-    console.log(result)
- return result
+function ratioList(size){
+    const result =[];
+    for(let i =1;i<=Math.sqrt(size); i++){
+        if(size % i=== 0) result.push([size/i,i])
+    }
+    return result
 }
+
+
