20aebc1a9a2bba5510f26e10909449ba130d049c
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-05-04 17:06:09 +0900

    [level 2] Title: 피로도, Time: 7.16 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,34 +1,37 @@
 function solution(k, dungeons) {
-    let answer = 0;
-    const permutations = getPermutations(dungeons,dungeons.length)
+    const permutations = getPermutations(dungeons,dungeons.length);
+    let answer =0;
+    console.log(permutations)
     
-    for( let i =0; i <permutations.length;i++){
-        const dungeon = permutations[i]
-        
-        let count =0;
-        let fatigue =k;
-        for (let j =0; j<dungeon.length;j++){
-            const target =dungeon[j]
-            if(fatigue>=target[0]){
-                count++
-                fatigue -=target[1]
+    for(let i=0; i<permutations.length;i++){
+        const dungeon = permutations[i];
+         
+        let count=0;
+        let fatique = k;
+        for(let j=0; j<dungeon.length; j++){
+            const target = dungeon[j];
+            if(fatique >= target[0]){
+                count++;
+                fatique -= target[1]
             }
         }
-        answer = Math.max(answer,count)
+        answer = Math.max(answer,count);
     }
     return answer
 }
 
-const getPermutations = (arr, num) => {
-  if (num === 1) return arr.map((v) => [v]);
 
-  const results = [];
-  arr.forEach((fixed, index, origin) => {
-    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
-    const permutaions = getPermutations(rest, num - 1);
-    const attached = permutaions.map((v) => [fixed, ...v]);
-    results.push(...attached);
-  });
+function getPermutations(arr,number) {
+    if(number ===1) return arr.map((el)=> [el]);
+    const result =[]
+    arr.forEach((fixed,index,origin)=>{
+        const rest = [...origin.slice(0,index),...origin.slice(index+1)];
+        const permutations = getPermutations(rest,number-1);
+        const attached = permutations.map((el)=>[fixed,...el]);
+        result.push(...attached)
+    })
+    return result    
+}
 
-  return results;
-};
+
+
