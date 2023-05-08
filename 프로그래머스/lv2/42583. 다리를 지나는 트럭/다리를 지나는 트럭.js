89e10bbe724b70744e145c8d653d23e1adf51ea4
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-05-08 23:40:18 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,31 +1,30 @@
 function solution(bridge_length, weight, truck_weights) {
-    let time = 0;
-    const bridge = new Array(bridge_length).fill(0)
-    let sumWeight =0;    
-
-    let nextTruck = truck_weights.shift()
-    bridge.pop()
-    bridge.unshift(nextTruck)
-    sumWeight +=nextTruck
+    let time =0;
+    const bridge =new Array(bridge_length).fill(0);
+    let weightSum =0;
+    
+    let nextTruck = truck_weights.shift();
+    bridge.pop();
+    bridge.unshift(nextTruck);
+    weightSum += nextTruck;
     time++
     
-    while(sumWeight){
+    while(weightSum){
         const passed = bridge.pop();
-        nextTruck = truck_weights.shift()
-        sumWeight -=passed
+        nextTruck = truck_weights.shift();
+        weightSum -= passed;
         
-        if(nextTruck === undefined) nextTruck = 0;
+        if(nextTruck === undefined) nextTruck =0;
         
-        if(nextTruck + sumWeight > weight){
+        if(weightSum + nextTruck > weight){
             truck_weights.unshift(nextTruck);
-            bridge.unshift(0)
+            bridge.unshift(0);
         }else{
-            bridge.unshift(nextTruck)
-            sumWeight +=nextTruck
+            bridge.unshift(nextTruck);
+            weightSum += nextTruck;
         }
         
-        time++
+        time++;
     }
-    
-    return time
+    return time;
 }
