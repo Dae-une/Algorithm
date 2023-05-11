b338b601720d74d5389ff2c9393d239ce3bd34e2
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-05-11 14:33:55 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,38 +1,42 @@
 function solution(fees, records) {
     const [baseTime,baseFee,overTime,overFee] = fees;
-    const carObj ={}
-    const lastTime = 23*60+59;
+    const lastTime = 23 * 60 +59;
+    const departure ={};
+    
+    records.forEach((list)=>{
+        const [time,number,action] = list.split(' ');
+        const minute = calcTimeToMinute(time);
+        
+        departure[number] = departure[number] || {totalTime:0,actionTime:null};
+        
+        if(action === 'IN'){
+            departure[number].actionTime = minute;
+        }else{
+            departure[number].totalTime += minute - departure[number].actionTime;
+            departure[number].actionTime = null;
+        }
+    })
+    
+    const carList = Object.keys(departure).sort((a,b)=> a-b);
     
     const calcPrice =(time)=>{
-        if(time <= baseTime) return baseFee
-        const calcTime = time - baseTime;
-        return baseFee + Math.ceil(calcTime / overTime) * overFee;
+        if(time <=baseTime) return baseFee
+        const calcTime = time -baseTime;
+        return baseFee + Math.ceil(calcTime/overTime) * overFee;
     }
-    
-    const calcTime =(time)=>{
-        const [HH,MM] = time.split(':')
-        return parseInt(HH) * 60 + parseInt(MM)
-    }
-    
-    records.forEach((record)=>{
-        const [time,id,type] = record.split(' ')
-        const minTime = calcTime(time)
-        
-        carObj[id] = carObj[id] || {times:0,state:null}
-        if(type === "IN") {
-            carObj[id].state = minTime
-        }else{
-            carObj[id].times += minTime-carObj[id].state
-            carObj[id].state=null
+
+    return carList.map((car)=>{
+        if(departure[car].actionTime !== null){
+            departure[car].totalTime += lastTime - departure[car].actionTime;
         }
+        return calcPrice(departure[car].totalTime);
     })
-    const ids = Object.keys(carObj).sort((a,b)=> +a-b)
+}
+
+function calcTimeToMinute(time){
+    const [HH,MM] = time.split(':')
+    const hour = parseInt(HH);
+    const minute = parseInt(MM);
     
-    
-    return ids.map((id)=>{
-        if(carObj[id].state !== null){
-            carObj[id].times += lastTime - carObj[id].state
-        }
-        return calcPrice(carObj[id].times)
-    })
+    return hour * 60 + minute
 }
