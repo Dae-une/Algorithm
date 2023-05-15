b8d504ba272d6bb48c6805303800d589cdb15bc6
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-05-15 19:12:12 +0900

    [level 2] Title: 땅따먹기, Time: 45.40 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
@@ -1,17 +1,13 @@
 function solution(land) {
-    let sum=0;
+    let sum = 0;
     
-    for(let i =0; i<land.length;i++){
-        for(let j =0; j< land[i].length;j++){
-            if(i===0){
-                continue;
-            }
+    for(let i =1; i<land.length;i++){
+        for(let j=0; j< land[i].length;j++){
             let arr = land[i-1].slice();
-            arr[j]=0;
-            land[i][j] += Math.max.apply(null,arr)
-            sum = Math.max(land[i][j],sum)
+            arr[j] = 0;
+            land[i][j] += Math.max(...arr);
+            sum = Math.max(sum,land[i][j])
         }
     }
-    
-    return sum
+    return sum;
 }
