a82fe9bfc3e9fa74db69f32fc483c276e52e8b3f
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-05-15 20:19:54 +0900

    [level 2] Title: 방문 길이, Time: 0.80 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
@@ -1,20 +1,21 @@
 function solution(dirs) {
+    var answer = 0;
     const move = { L: [-1, 0], R: [1, 0], U: [0, 1], D: [0, -1] };
     let now =[0,0];
-    const visited= new Set();
+    const visited =new Set();
     
     for(let i =0; i<dirs.length;i++){
-       let nowX =now[0] + move[dirs[i]][0];
-       let nowY =now[1] + move[dirs[i]][1];
+        const nowX = now[0] + move[dirs[i]][0];
+        const nowY = now[1] + move[dirs[i]][1];
         
-        if(nowX >5 ||nowX <-5 || nowY >5 || nowY <-5 ) continue;
-    
-        visited.add(""+now[0] +now[1]+nowX+nowY)
-        visited.add(""+nowX+nowY+now[0]+now[1])
+        if(nowX > 5 || nowX <-5 || nowY >5 || nowY <-5) continue;
         
-        now =[nowX,nowY]
+        visited.add(''+now[0] +now[1] + nowX+nowY);
+        visited.add(''+nowX+nowY+now[0]+now[1]);
+        now = [nowX,nowY]
     }
-    return visited.size/2
-
+    
+    return visited.size/2;
 }
+
     
